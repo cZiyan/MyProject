3ffc681c6a73e21ad97aa9ef1c4166769d1d1055
jeff850524 <jeff850524@yahoo.com.tw>
2023-02-20 18:12:18 +0800

    加入專案檔案。

---
+++ MyProject/Controllers/ManageController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyProject.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MyProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ManageController : ControllerBase
+    {
+
+        private NorthwindContext _context;
+
+        public ManageController(NorthwindContext dbContext)
+        {
+            _context = dbContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> GetTodoItemList()
+        {
+            return await _context.Products.ToListAsync();
+        }
+
+        
+
+    }
+}
